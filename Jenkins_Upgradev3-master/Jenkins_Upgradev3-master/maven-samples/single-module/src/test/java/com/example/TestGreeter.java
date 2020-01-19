de062dc5e14f7f324aba1def5878b04ce01d4117
rishi.kant.prasad <mirishikant1@gmail.com>
2020-01-20 01:41:55 +0530

    GitPoll

--- Jenkins_Upgradev3-master/Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3-master/Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,10 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+ @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, I am Rishi";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
