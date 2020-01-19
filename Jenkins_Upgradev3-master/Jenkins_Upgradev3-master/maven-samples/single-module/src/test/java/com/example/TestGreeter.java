34d3f98cf1405500aec391995b2c4e4f594f4f54
rishi.kant.prasad <mirishikant1@gmail.com>
2020-01-20 01:47:14 +0530

    Rename Method

--- Jenkins_Upgradev3-master/Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3-master/Jenkins_Upgradev3-master/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -38,7 +38,7 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
  @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void GreetingMessage() {
     String someone = "World, I am Rishi";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
